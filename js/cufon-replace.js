68855a420aefb564d327339fada312dfb45e9754
MOLLETI HAREESH <m.hareesh2504@gmail.com>
2023-10-09 10:41:02 +0530

    Windows

--- js/cufon-replace.js
+++
@@ -1,3 +0,0 @@
-Cufon.replace('#header .row-1 .fright ul li', { fontFamily: 'Myriad Pro Regular' });
-Cufon.replace('h3', { fontFamily: 'Myriad Pro Light' });
-Cufon.replace('h3 b', { fontFamily: 'Myriad Pro Regular' });
---
+++ search.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <link rel="icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" >
+    <link rel="stylesheet" href="css/responsive.css" >
+    <link rel="stylesheet" href="css/style.css" >
+    <!-- font-awesome font -->
+    <link rel="stylesheet" href="font/font-awesome.css" type="text/css" media="screen">
+    <!-- fontello font -->
+		<script src="js/jquery.js"></script>
+        <script src="js/jquery.easing.1.3.js"></script>
+        <script src="js/jquery-migrate-1.1.1.js"></script>
+        <script src="js/superfish.js"></script>
+        <script src="js/jquery.mobilemenu.js"></script>
+         
+        <script src="search/search.js"></script>  
+             
+      <!--[if lt IE 8]>
+          <div style='text-align:center'><a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://www.theie6countdown.com/images/upgrade.jpg"border="0"alt=""/></a></div>  
+      <![endif]-->
+  <!--[if lt IE 9]>
+
+    <link rel="stylesheet" href="css/ie.css" >
+    <script src="assets/js/html5shiv.js"></script>
+  <![endif]-->
+</head>
+<body class="sub-page">
+<div class="bg">
+  <div class="black-bg"></div>
+  <header>  
+    <div class="container">
+      <div class="row">
+          <div class="span12 header-block">
+              <h1 class="brand"><a href="index.html"><strong>42</strong><span>Windows</span></a></h1> 
+              <div class="navbar navbar_ clearfix">
+                <div class="navbar-inner">                                                 
+                  <div class="nav-collapse nav-collapse_ collapse">
+                    <ul class="nav sf-menu clearfix">
+                      <li class="sub-menu"><a href="index.html">Home</a><span>|</span>
+                      	<ul>
+                          <li><a href="#">Corporate profile</a></li>
+                          <li><a href="#">What's new</a></li>
+                          <li><a href="#">Products</a></li>
+                          <li><a href="#">Services</a></li>
+                          <li><a href="#">Test results</a></li>
+                          <li><a href="#">Become a dealer</a></li>
+                        </ul>
+                      </li>
+                      <li><a href="index-1.html">About</a><span>|</span></li>
+                      <li><a href="index-2.html">Services</a><span>|</span></li>
+                      <li><a href="index-3.html">Blog</a><span>|</span></li>
+                      <li><a href="index-4.html">Contacts</a></li>
+                    </ul>
+                  </div>
+                </div>
+            </div>
+          </div> 
+        </div> 
+      </div> 
+  </header>
+  
+
+  <!--==============================content=================================-->
+  <div id="content"> 
+    <div class="container">
+       <div class="row">
+          <div class="span12">
+              <h3>Search result:</h3>
+              <div id="search-results"></div>
+          </div>
+       </div>      
+   </div>  
+  </div>          
+</div>  
+<!--==============================footer=================================-->
+  <footer>
+    <div class="container">
+        <div class="row">
+            <div class="span12">
+            	<ul class="social-icons">
+                	<li><a href="#" class="icon-facebook"></a></li>
+                    <li><a href="#" class="icon-google-plus"></a></li>
+                    <li><a href="#" class="icon-rss"></a></li>
+                    <li><a href="#" class="icon-pinterest"></a></li>
+                    <li><a href="#" class="icon-linkedin"></a></li>
+                </ul>
+            </div>  
+        </div>  
+    	<div class="row">
+            <div class="span12 footer-text">42 windows &copy; 2013 &bull; <a href="index-5.html">Privacy Policy</a></div>  
+        </div> 
+    </div>
+ </footer>
+<script src="js/bootstrap.js"></script>
+</body>
+</html>
